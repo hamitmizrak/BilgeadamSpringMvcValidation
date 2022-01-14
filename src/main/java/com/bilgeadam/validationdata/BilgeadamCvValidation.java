86d02ae9630f45d266f06d0e821ca117c002f497
hamitmizrak <hamitmizrak@gmail.com>
2022-01-14 14:17:19 +0300

    pom.xml revize

---
+++ src/main/java/com/bilgeadam/validationdata/BilgeadamCvValidation.java
@@ -0,0 +1,17 @@
+package com.bilgeadam.validationdata;
+
+import java.sql.Date;
+
+public class BilgeadamCvValidation {
+
+    //object variable
+    private String userName;
+    private String userSurname;
+    private String userAddress;
+    private String userEmailAddress;
+    private int userAge;
+    private String userPhone;
+    private Date creationDate;
+
+
+}
