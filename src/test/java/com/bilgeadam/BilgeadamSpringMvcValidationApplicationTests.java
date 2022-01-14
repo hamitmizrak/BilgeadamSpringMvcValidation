564c0fd17f9221ec920531ac987d105d1a5a40f9
hamitmizrak <hamitmizrak@gmail.com>
2022-01-14 14:02:17 +0300

    Spring MVC Thymeleaf template and validation first commit

---
+++ src/test/java/com/bilgeadam/BilgeadamSpringMvcValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilgeadamSpringMvcValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
