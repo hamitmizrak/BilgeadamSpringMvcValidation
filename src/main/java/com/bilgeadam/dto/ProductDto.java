a18b27cd2dc9b0adc28a8e2cd77a2d15d363b5e4
hamitmizrak <hamitmizrak@gmail.com>
2022-02-03 11:55:02 +0300

    dynamicjson produces pathvariable pathvariablelist

---
+++ src/main/java/com/bilgeadam/dto/ProductDto.java
@@ -0,0 +1,17 @@
+package com.bilgeadam.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductDto {
+
+    private Long productId;
+    private String productName;
+    private String productTrade;
+}
--- src/main/java/com/bilgeadam/services/ProductServices.java
+++ src/main/java/com/bilgeadam/services/ProductServices.java
@@ -1,14 +1,57 @@
 package com.bilgeadam.services;
 
+import com.bilgeadam.dto.ProductDto;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.MediaType.*;
 
 @RestController
 public class ProductServices {
 
-    // localhost:8090/product
-    @GetMapping("/product")
-    public String getProducts(){
+    // localhost:8090/rest/manueljson
+    @GetMapping("/rest/manueljson")
+    public String getManuelJson() {
         return "{ \"adı\": \"Hamit Mızrak\"}";
     }
+
+    // localhost:8090/rest/dynamicjson
+    @GetMapping("/rest/dynamicjson")
+    public ProductDto getDynamicsJson() {
+        ProductDto productDto = ProductDto.builder().productId(0L).productName("Bilgisayar").productTrade("marka").build();
+        return productDto;
+    }
+
+    // localhost:8090/rest/produces
+    @GetMapping(value = "/rest/produces", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ProductDto getProduces() {
+        ProductDto productDto = ProductDto.builder().productId(0L).productName("Bilgisayar2").productTrade("marka2").build();
+        return productDto;
+    }
+
+    // localhost:8090/rest/pathvariable/urunadi
+    @GetMapping(value = "/rest/pathvariable/{productname}")
+    public ProductDto getPathParam(@PathVariable("productname") String product_name) {
+        ProductDto productDto = ProductDto.builder().productId(0L).productName(product_name).productTrade("marka3").build();
+        return productDto;
+    }
+
+
+    // localhost:8090/rest/pathvariablelist/urunadi
+    @GetMapping(value = "/rest/pathvariablelist/{productname}")
+    public List<ProductDto>  getPathParamList(@PathVariable("productname") String product_name) {
+        List<ProductDto> productDtoList=new ArrayList<>();
+        productDtoList.add(ProductDto.builder().productId(0L).productName(product_name).productTrade("marka4").build());
+        productDtoList.add(ProductDto.builder().productId(0L).productName(product_name).productTrade("marka44").build());
+        productDtoList.add(ProductDto.builder().productId(0L).productName(product_name).productTrade("marka444").build());
+        return productDtoList;
+    }
+
 }
