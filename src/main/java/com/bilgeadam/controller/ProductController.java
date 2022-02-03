a69ba36da3514aef9d4b92b749013ce96bb68384
hamitmizrak <hamitmizrak@gmail.com>
2022-02-03 15:21:50 +0300

    ResponseEntity

--- src/main/java/com/bilgeadam/controller/ProductController.java
+++ src/main/java/com/bilgeadam/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.bilgeadam.controller;
 
 import com.bilgeadam.dto.ProductDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,14 +47,28 @@ public class ProductController {
 
 
     // http://localhost:8090/client/controller/pathvariable/productdto/denemeurun
+    //getForObject
     @GetMapping("/client/controller/pathvariable/productdto/{urun_adi}")
     @ResponseBody
-    public String getProductServicesObject(@PathVariable("urun_adi") String urunAdim44) {
+    public ProductDto getProductServicesObject(@PathVariable("urun_adi") String urunAdim44) {
         String URL = "http://localhost:8090/rest/pathvariable/"+urunAdim44;
-
         // restten veri almak istiyorsam;
         RestTemplate restTemplate=new RestTemplate();
         ProductDto productDto= restTemplate.getForObject(URL, ProductDto.class);
-        return "veriler55:"+ productDto;
+        return  productDto;
+    }
+
+
+    // http://localhost:8090/client/controller/pathvariable/productdto/special/denemeurun66
+    @GetMapping("/client/controller/pathvariable/productdto/special/{urun_adi}")
+    @ResponseBody
+    public ProductDto getProductServicesSupLevel(@PathVariable("urun_adi") String urunAdim44) {
+        String URL = "http://localhost:8090/rest/pathvariable/"+urunAdim44;
+        // restten veri almak istiyorsam;
+        RestTemplate restTemplate=new RestTemplate();
+        ProductDto productDto= restTemplate.getForObject(URL, ProductDto.class);
+        ResponseEntity<ProductDto> responseEntity= restTemplate.exchange(URL, HttpMethod.GET, HttpEntity.EMPTY,ProductDto.class);
+        ProductDto productDto2=responseEntity.getBody();
+        return productDto2;
     }
 }
